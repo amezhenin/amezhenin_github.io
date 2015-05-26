3439a983dc008cc458e9979d5d1cf5c65aa84173
Artem Mezhenin <a.mezhenin@gmail.com>
2015-05-26 22:28:47 +0700

    plugin

--- _plugins/popular.rb
+++ _plugins/popular.rb
@@ -1,13 +1,17 @@
-module Popular
-    def popular(tags, text)
-        if tags.include? "popular"
-            "<b>" +
-            text +
-            "</b>"
-        else 
-            text
+require 'pp'
+module Jekyll
+    module PopularFilter
+
+        def popular(tags, text)
+            if tags.include? "popular"
+                "<b>" +
+                text +
+                "</b>"
+            else 
+                text
+            end
         end
     end
 end
 
-Liquid::Template.register_filter(Popular)
+Liquid::Template.register_filter(Jekyll::PopularFilter)
